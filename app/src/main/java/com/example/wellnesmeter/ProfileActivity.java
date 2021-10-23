d6aa9c604d9954ba426284a0f43e6f39f44693a4
Suraj Kumar Modi <76468931+skmodi649@users.noreply.github.com>
2021-10-23 12:21:46 +0530

    Scroll View commit

--- app/src/main/java/com/example/wellnesmeter/ProfileActivity.java
+++ app/src/main/java/com/example/wellnesmeter/ProfileActivity.java
@@ -2,7 +2,6 @@ package com.example.wellnesmeter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -14,15 +13,13 @@ public class ProfileActivity extends AppCompatActivity {
 
     private Button logout;
     private Button input;
-    private Button bmi;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
         logout = (Button) findViewById(R.id.logout);
-        input = (Button) findViewById(R.id.input);
-        bmi = (Button) findViewById(R.id.bmichart);
+        input = (Button) findViewById(R.id.bmi);
         logout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -34,16 +31,6 @@ public class ProfileActivity extends AppCompatActivity {
             @Override
             public void onClick(View view){
                 startActivity(new Intent(ProfileActivity.this , InputActivity.class));
-            }
-        });
-
-        bmi.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent intent = new Intent();
-                intent.setAction(Intent.ACTION_VIEW);
-                intent.addCategory(Intent.CATEGORY_BROWSABLE);
-                intent.setData(Uri.parse("https://www.cancer.org/cancer/cancer-causes/diet-physical-activity/body-weight-and-cancer-risk/adult-bmi.html"));
-                startActivity(intent);
             }
         });
     }
