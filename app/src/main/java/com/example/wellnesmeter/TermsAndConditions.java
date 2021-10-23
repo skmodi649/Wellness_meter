35c4503d1accaf67fbee25ae7161f8364aa1ff51
Suraj Kumar Modi <76468931+skmodi649@users.noreply.github.com>
2021-10-24 02:31:23 +0530

    terms and conditions commit

--- app/src/main/java/com/example/wellnesmeter/TermsAndConditions.java
+++ app/src/main/java/com/example/wellnesmeter/TermsAndConditions.java
@@ -1,14 +1,25 @@
 package com.example.wellnesmeter;
 
+import android.content.Intent;
 import android.os.Bundle;
-
+import android.widget.Button;
+import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class TermsAndConditions extends AppCompatActivity {
+public class TermsAndConditions extends AppCompatActivity implements View.OnClickListener{
+    private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_conditions);
+        button = (Button) findViewById(R.id.agree);
+        button.setOnClickListener(this);
+
+    }
+    @Override
+    public void onClick(View view)
+    {
+        startActivity(new Intent(TermsAndConditions.this , ProfileActivity.class));
     }
 }
