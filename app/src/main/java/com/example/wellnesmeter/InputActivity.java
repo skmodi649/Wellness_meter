efbbdb62cc6e7d8e1382b102b048f877c22c01dd
Suraj Kumar Modi <76468931+skmodi649@users.noreply.github.com>
2021-10-26 23:59:19 +0530

    Further development

--- app/src/main/java/com/example/wellnesmeter/InputActivity.java
+++ app/src/main/java/com/example/wellnesmeter/InputActivity.java
@@ -1,14 +1,31 @@
 package com.example.wellnesmeter;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.text.Editable;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class InputActivity extends AppCompatActivity {
 
+    private EditText weight;
+    private EditText height;
+    private TextView bmires;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_input);
+
+        weight = (EditText) findViewById(R.id.wt);
+        height = (EditText) findViewById(R.id.ht);
+        bmires = (TextView) findViewById(R.id.bmires);
+
+        double wt = Double.parseDouble(String.valueOf(weight.getText()));
+        double ht = Double.parseDouble(String.valueOf(height.getText()));
+
+        double bmi = wt / (ht*ht);
+
+
+
     }
 }
