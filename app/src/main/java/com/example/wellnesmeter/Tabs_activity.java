dd4a8f08f624d82601b314d7eaff53c11d42d489
Suraj Kumar Modi <76468931+skmodi649@users.noreply.github.com>
2021-11-12 12:29:48 +0530

    Tabs activity setup 1

---
+++ app/src/main/java/com/example/wellnesmeter/Tabs_activity.java
@@ -0,0 +1,23 @@
+package com.example.wellnesmeter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Tabs_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tabs);
+
+        Button hic = findViewById(R.id.heal);
+        Button aqi = findViewById(R.id.aqi);
+        Button covid = findViewById(R.id.covid);
+
+        hic.setOnClickListener(view -> startActivity(new Intent(Tabs_activity.this , TermsAndConditions.class)));
+    }
+}
