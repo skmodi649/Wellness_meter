9b071524e68a41a9837df08cdb9f8d900b774783
Suraj Kumar Modi <76468931+skmodi649@users.noreply.github.com>
2021-11-21 17:50:29 +0530

    tabs activity commit

--- app/src/main/java/com/example/wellnesmeter/Tabs_activity.java
+++ app/src/main/java/com/example/wellnesmeter/Tabs_activity.java
@@ -4,7 +4,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 
 public class Tabs_activity extends AppCompatActivity {
