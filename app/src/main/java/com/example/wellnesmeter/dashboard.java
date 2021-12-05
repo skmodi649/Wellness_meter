2a21f763e0ddca73aab35216dcacb37bd7498f08
Suraj Kumar Modi <76468931+skmodi649@users.noreply.github.com>
2021-12-06 03:34:19 +0530

    Rest api request and volley part done commit

--- app/src/main/java/com/example/wellnesmeter/dashboard.java
+++ app/src/main/java/com/example/wellnesmeter/dashboard.java
@@ -3,12 +3,101 @@ package com.example.wellnesmeter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ScrollView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.leo.simplearcloader.SimpleArcDialog;
+import com.leo.simplearcloader.SimpleArcLoader;
+
+import org.eazegraph.lib.charts.PieChart;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class dashboard extends AppCompatActivity {
+
+    TextView tvCases,tvRecovered,tvCritical,tvActive,tvTodayCases,tvTotalDeaths,tvTodayDeaths,tvAffectedCountries;
+    SimpleArcLoader simpleArcLoader;
+    PieChart pieChart;
+    ScrollView scrollView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        tvCases = findViewById(R.id.tvCases);
+        tvRecovered = findViewById(R.id.tvRecovered);
+        tvCritical = findViewById(R.id.tvCritical);
+        tvActive = findViewById(R.id.tvActive);
+        tvTodayCases = findViewById(R.id.tvTodayCases);
+        tvTotalDeaths = findViewById(R.id.tvTotalDeaths);
+        tvTodayDeaths = findViewById(R.id.tvTodayDeaths);
+        tvAffectedCountries = findViewById(R.id.tvAffectedCountries);
+
+        simpleArcLoader = findViewById(R.id.loader);
+        scrollView = findViewById(R.id.scrollView);
+        pieChart = findViewById(R.id.pieChart);
+
+
+        //Function to fetch data from the Rest API
+        fetchData();
+
+
+    }
+
+
+    // Now we are going to use the Rest API for the covid 19
+    private void fetchData(){
+
+        String url = "https://corona.lmao.ninja/v2/all/";
+
+        simpleArcLoader.start();
+
+        StringRequest request = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                // Now we have to handle the JSON text
+
+                try {
+                    JSONObject jsonObject = new JSONObject(response.toString());
+
+
+
+
+
+
+
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(dashboard.this, error.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(request);
+
+
+
+
+    }
+
+    public void goTrackCountries(View view) {
+
     }
 }
