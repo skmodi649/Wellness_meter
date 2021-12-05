d58d9cb6efb6ce5b43a64e4031044125b05e405c
Suraj Kumar Modi <76468931+skmodi649@users.noreply.github.com>
2021-12-05 15:52:58 +0530

    tabs activity commit done

--- app/src/main/java/com/example/wellnesmeter/TermsAndConditions.java
+++ app/src/main/java/com/example/wellnesmeter/TermsAndConditions.java
@@ -26,7 +26,7 @@ public class TermsAndConditions extends AppCompatActivity implements View.OnClic
         switch (view.getId())
         {
             case R.id.agree:
-                startActivity(new Intent(TermsAndConditions.this , InputActivity.class));
+                startActivity(new Intent(TermsAndConditions.this , Tabs_activity.class));
                 break;
 
             case R.id.decline:
