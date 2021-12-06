a8cc962d65e4c77664faa11518b1b02bec3e95c6
Suraj Kumar Modi <76468931+skmodi649@users.noreply.github.com>
2021-12-06 10:56:22 +0530

    Affected countries activity created commit

---
+++ app/src/main/java/com/example/wellnesmeter/AffectedCountries.java
@@ -0,0 +1,14 @@
+package com.example.wellnesmeter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AffectedCountries extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_affected_countries);
+    }
+}
