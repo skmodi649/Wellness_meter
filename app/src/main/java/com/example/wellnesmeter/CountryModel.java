69a9829cb11d677b81d8d6567f3cdc022a280e14
Suraj Kumar Modi <76468931+skmodi649@users.noreply.github.com>
2021-12-06 12:09:20 +0530

    class for custom listview created commit

---
+++ app/src/main/java/com/example/wellnesmeter/CountryModel.java
@@ -0,0 +1,113 @@
+package com.example.wellnesmeter;
+
+public class CountryModel {
+    private String flag,country,cases,todayCases,deaths,todayDeaths,recovered,activeCases,critical,population,continent;
+
+    // Default constructor
+    public CountryModel() {
+
+    }
+
+    // Parameterised constructor
+    public CountryModel(String flag, String country, String cases, String todayCases, String deaths, String todayDeaths, String recovered, String activeCases, String critical, String population, String continent) {
+        this.flag = flag;
+        this.country = country;
+        this.cases = cases;
+        this.todayCases = todayCases;
+        this.deaths = deaths;
+        this.todayDeaths = todayDeaths;
+        this.recovered = recovered;
+        this.activeCases = activeCases;
+        this.critical = critical;
+        this.population = population;
+        this.continent = continent;
+    }
+
+    public String getFlag() {
+        return flag;
+    }
+
+    public void setFlag(String flag) {
+        this.flag = flag;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getCases() {
+        return cases;
+    }
+
+    public void setCases(String cases) {
+        this.cases = cases;
+    }
+
+    public String getTodayCases() {
+        return todayCases;
+    }
+
+    public void setTodayCases(String todayCases) {
+        this.todayCases = todayCases;
+    }
+
+    public String getDeaths() {
+        return deaths;
+    }
+
+    public void setDeaths(String deaths) {
+        this.deaths = deaths;
+    }
+
+    public String getTodayDeaths() {
+        return todayDeaths;
+    }
+
+    public void setTodayDeaths(String todayDeaths) {
+        this.todayDeaths = todayDeaths;
+    }
+
+    public String getRecovered() {
+        return recovered;
+    }
+
+    public void setRecovered(String recovered) {
+        this.recovered = recovered;
+    }
+
+    public String getActiveCases() {
+        return activeCases;
+    }
+
+    public void setActiveCases(String activeCases) {
+        this.activeCases = activeCases;
+    }
+
+    public String getCritical() {
+        return critical;
+    }
+
+    public void setCritical(String critical) {
+        this.critical = critical;
+    }
+
+    public String getPopulation() {
+        return population;
+    }
+
+    public void setPopulation(String population) {
+        this.population = population;
+    }
+
+    public String getContinent() {
+        return continent;
+    }
+
+    public void setContinent(String continent) {
+        this.continent = continent;
+    }
+}
