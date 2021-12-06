e507af70bfc8594e41a6ffbcc48cc6fad858f5bf
Suraj Kumar Modi <76468931+skmodi649@users.noreply.github.com>
2021-12-06 12:02:07 +0530

    Basic layout of the affected countries activity done commit

--- app/src/main/java/com/example/wellnesmeter/AffectedCountries.java
+++ app/src/main/java/com/example/wellnesmeter/AffectedCountries.java
@@ -3,12 +3,25 @@ package com.example.wellnesmeter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import com.leo.simplearcloader.SimpleArcLoader;
 
 public class AffectedCountries extends AppCompatActivity {
 
+    EditText editSearch;
+    ListView listView;
+    SimpleArcLoader simpleArcLoader;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_affected_countries);
+
+        editSearch = findViewById(R.id.editSearch);
+        listView = findViewById(R.id.listView);
+        simpleArcLoader = findViewById(R.id.loaderla);
+
+
     }
 }
