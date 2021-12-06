6fe3c1f1b704b8b3eae25e0405d89323b3e5bada
Suraj Kumar Modi <76468931+skmodi649@users.noreply.github.com>
2021-12-06 12:16:08 +0530

    xml layout for the custom adapter done commit

---
+++ app/src/main/java/com/example/wellnesmeter/MyCustomAdapter.java
@@ -0,0 +1,18 @@
+package com.example.wellnesmeter;
+
+import android.content.Context;
+import android.widget.ArrayAdapter;
+
+import androidx.annotation.NonNull;
+
+import java.util.List;
+
+public class MyCustomAdapter extends ArrayAdapter<CountryModel> {
+    private Context context;
+    private List<CountryModel> list;
+
+
+    public MyCustomAdapter(Context context, List<CountryModel> countryModelList) {
+        super(context, );
+    }
+}
