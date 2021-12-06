16f42462da8109625f3bbce23b995b5c9f9a6bcd
Suraj Kumar Modi <76468931+skmodi649@users.noreply.github.com>
2021-12-06 14:29:23 +0530

    Individual countries' info fully done commit

--- app/src/main/java/com/example/wellnesmeter/DetailActivity.java
+++ app/src/main/java/com/example/wellnesmeter/DetailActivity.java
@@ -22,6 +22,25 @@ public class DetailActivity extends AppCompatActivity {
 
         tvCountry = findViewById(R.id.tvCountryName);
         tvCases = findViewById(R.id.tvCases);
+        tvCritical = findViewById(R.id.tvCritical);
+        tvDeaths = findViewById(R.id.tvTotalDeaths);
+        tvTodayDeaths = findViewById(R.id.tvTodayDeaths);
+        tvRecovered = findViewById(R.id.tvRecovered);
+        tvActive = findViewById(R.id.tvActive);
+        tvTodayCases = findViewById(R.id.tvTodayCases);
+        tvPopulation = findViewById(R.id.tvPopulation);
+        tvContinent = findViewById(R.id.tvContinent);
+
+        tvCountry.setText(AffectedCountries.countryModelList.get(positionCountry).getCountry());
+        tvCases.setText(AffectedCountries.countryModelList.get(positionCountry).getCases());
+        tvRecovered.setText(AffectedCountries.countryModelList.get(positionCountry).getRecovered());
+        tvCritical.setText(AffectedCountries.countryModelList.get(positionCountry).getCritical());
+        tvActive.setText(AffectedCountries.countryModelList.get(positionCountry).getActiveCases());
+        tvTodayCases.setText(AffectedCountries.countryModelList.get(positionCountry).getTodayCases());
+        tvDeaths.setText(AffectedCountries.countryModelList.get(positionCountry).getDeaths());
+        tvTodayDeaths.setText(AffectedCountries.countryModelList.get(positionCountry).getTodayDeaths());
+        tvPopulation.setText(AffectedCountries.countryModelList.get(positionCountry).getPopulation());
+        tvContinent.setText(AffectedCountries.countryModelList.get(positionCountry).getContinent());
 
 
     }
