43d587921e65792385b53c26664643176542a240
Surmo <skmodi20bce2835@gmail.com>
2022-11-13 14:25:57 +0530

    Health Tabbed Activity Fragment created commit

--- app/src/main/java/com/example/wellnesmeter/ui/main/SectionsPagerAdapter.java
+++ app/src/main/java/com/example/wellnesmeter/ui/main/SectionsPagerAdapter.java
@@ -17,7 +17,7 @@ import com.example.wellnesmeter.R;
 public class SectionsPagerAdapter extends FragmentPagerAdapter {
 
     @StringRes
-    private static final int[] TAB_TITLES = new int[]{R.string.tab_text_1, R.string.tab_text_2};
+    private static final int[] TAB_TITLES = new int[]{R.string.tab_text_1, R.string.tab_text_2, R.string.tab_text_3};
     private final Context mContext;
 
     public SectionsPagerAdapter(Context context, FragmentManager fm) {
@@ -40,7 +40,7 @@ public class SectionsPagerAdapter extends FragmentPagerAdapter {
 
     @Override
     public int getCount() {
-        // Show 2 total pages.
-        return 2;
+        // Show 3 total pages.
+        return 3;
     }
 }
