140652439d34c0cee29610b02ff50b1b7a0cd2c7
Surmo <skmodi20bce2835@gmail.com>
2023-07-05 03:01:04 +0530

    Tabs Activity update

--- app/src/main/java/com/example/wellnesmeter/Tabs_activity.java
+++ app/src/main/java/com/example/wellnesmeter/Tabs_activity.java
@@ -13,24 +13,17 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class Tabs_activity extends AppCompatActivity {
     private FirebaseAuth mAuth;
-    private ImageButton imageButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tabs);
 
-        Button hic = findViewById(R.id.heal);
-        Button health_history = findViewById(R.id.health_history);
+        Button hic = findViewById(R.id.score);
+        Button health_history = findViewById(R.id.sensing);
         mAuth = FirebaseAuth.getInstance();
 
         hic.setOnClickListener(view -> startActivity(new Intent(Tabs_activity.this , InputActivity.class)));
         health_history.setOnClickListener(view -> startActivity(new Intent(Tabs_activity.this , com.example.wellnesmeter.health_history.class)));
-        imageButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(Tabs_activity.this , MainActivity.class));
-            }
-        });
     }
      @Override
     protected void onStart() {
---
+++ app/src/main/java/com/example/wellnesmeter/slogin.java
@@ -0,0 +1,14 @@
+package com.example.wellnesmeter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class slogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_slogin);
+    }
+}
