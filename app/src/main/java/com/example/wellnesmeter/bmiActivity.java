513c866e5abaff2422b0b5f40ce5fdae061df8be
Surmo <skmodi20bce2835@gmail.com>
2023-07-05 23:02:33 +0530

    Details activity commit

--- app/src/main/java/com/example/wellnesmeter/bmiActivity.java
+++ app/src/main/java/com/example/wellnesmeter/bmiActivity.java
@@ -8,8 +8,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import org.w3c.dom.Text;
-
 public class bmiActivity extends AppCompatActivity {
 
     @Override
