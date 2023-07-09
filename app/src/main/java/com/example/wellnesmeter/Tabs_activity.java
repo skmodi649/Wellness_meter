f56dfa0053ddccfc0e21fc967cffc306569b9b1e
Surmo <skmodi20bce2835@gmail.com>
2023-07-09 23:44:13 +0530

    Navigation Drawer commit

--- app/src/main/java/com/example/wellnesmeter/Tabs_activity.java
+++ app/src/main/java/com/example/wellnesmeter/Tabs_activity.java
@@ -1,23 +1,87 @@
 package com.example.wellnesmeter;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 
-import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.Toolbar;
 
 import com.example.wellnesmeter.tutorial.tutorial_starting;
+import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class Tabs_activity extends AppCompatActivity {
     private FirebaseAuth mAuth;
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    Toolbar toolbar;
+    ActionBarDrawerToggle actionBarDrawerToggle;
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(actionBarDrawerToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maintab);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.navigationView);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.menu_Open,  R.string.menu_Close);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                // Switch and case statements to be able to map buttons in navigation drawer to their respective actions
+                switch (item.getItemId()){
+                    case R.id.nav_home:
+                        Log.i("MENU_DRAWER_TAG", "Home item has been clicked!");
+                        drawerLayout.closeDrawer(GravityCompat.START);
+                        break;
+
+                    case R.id.nav_share:
+                        Log.i("MENU_DRAWER_TAG", "Share item has been clicked!");
+                        drawerLayout.closeDrawer(GravityCompat.START);
+                        break;
+
+                    case R.id.nav_logout:
+                        Log.i("MENU_DRAWER_TAG", "Log out item has been clicked!");
+                        drawerLayout.closeDrawer(GravityCompat.START);
+                        break;
+
+                    case R.id.nav_settings:
+                        Log.i("MENU_DRAWER_TAG", "Settings item has been clicked!");
+                        drawerLayout.closeDrawer(GravityCompat.START);
+                        break;
+
+                    case R.id.nav_users:
+                    Log.i("MENU_DRAWER_TAG", "Users item has been clicked!");
+                    drawerLayout.closeDrawer(GravityCompat.START);
+                    break;
+                }
+
+
+                return true;
+            }
+        });
+
+
 
         ImageView hic = findViewById(R.id.score);
         ImageView health_sensing = findViewById(R.id.sensing);
